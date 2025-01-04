a7464a0fadc4a96bdacbff549d627097fa7b9fd9
Giada Unger <giada.unger02@gmail.com>
2025-01-04 13:08:52 +0100

    Added a secons schema and updated the requirements file

--- schemas.py
+++ schemas.py
@@ -1,6 +1,11 @@
 from pydantic import BaseModel
 
+class IngredientSchema(BaseModel):
+  id: int
+  ingredient: str
+
 class ProductSchema(BaseModel):
   id: int
   product_name: str
   product_img: str
+  ingredients: list[IngredientSchema]
