7314ffbac2a091b27226b035e8fb361765368c0a
Giada Unger <giada.unger02@gmail.com>
2025-01-06 11:42:02 +0100

    Added middleware

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException, Depends, status, Request
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from app.db_setup import get_db, init_db
 from sqlalchemy.orm import Session, joinedload, selectinload
@@ -11,6 +12,21 @@ async def lifespan(app: FastAPI):
   yield
 
 app =  FastAPI(lifespan=lifespan)
+
+# Middleware
+origin = [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://localhost:5174"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origin,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/product", status_code=200)
 def list_products(db: Session = Depends(get_db)):
