2fd1b733fc02524235c6126d43fb08f031f7f2a4
Giada Unger <giada.unger02@gmail.com>
2025-03-09 19:48:14 +0100

    Fixed typo in url

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ origin = [
     "http://localhost:3000",
     "http://localhost:5173",
     "http://localhost:5174",
-    "https://mixskincare.netlify.app/"
+    "https://mixskincare.netlify.app"
 ]
 
 app.add_middleware(
